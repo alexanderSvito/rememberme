b014b9d599fd0be3993ebc3c69cd5924a386fe7f
Alexander Svito <alexandervirk@gmail.com>
2019-04-16 17:06:24 +0300

    fix configs

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import telebot
+import config
 
 
+bot = telebot.TeleBot(config.TOKEN)
 
-TOKEN = '783293998:AAHoM0ehXwv7RSIDnDj1j7WlME3x1U1mtUk'
-
-bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    bot.send_message(message.chat.id, "Привет, я помогу тебе запомнить что-то по принципу 'Якорь-Отклик'")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    print(message.text)
     bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
